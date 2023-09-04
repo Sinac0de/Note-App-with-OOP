e8a36dc0ce8d2f0725ee10462ce498143ae1a0c7
Sina Moradian <sinacsharp@gmail.com>
2023-09-05 02:56:22 +0330

    Add hint in the new note body

--- js/App.js
+++ js/App.js
@@ -2,65 +2,64 @@ import NotesAPI from "./NotesAPI.js";
 import NotesView from "./NotesView.js";
 
 export default class App {
+  constructor(root) {
+    this.notes = [];
+    this.activeNote = null;
+    this.view = new NotesView(root, this._handlers());
+    this._refreshNotes();
+  }
 
-    constructor(root) {
-        this.notes = [];
-        this.activeNote = null;
-        this.view = new NotesView(root, this._handlers());
+  _refreshNotes() {
+    const notes = NotesAPI.getAllNotes();
+
+    //set notes:
+    this._setNotes(notes);
+
+    //set active note:
+    if (notes.length > 0) {
+      this._setActiveNote(notes[0]);
+    }
+  }
+
+  _setNotes(notes) {
+    this.notes = notes;
+    this.view.updateNoteList(notes);
+    this.view.updateNotePreviewVisiblity(notes.length > 0);
+  }
+
+  _setActiveNote(note) {
+    this.activeNote = note;
+    this.view.updateActiveNote(note);
+  }
+
+  _handlers() {
+    return {
+      onNoteAdd: () => {
+        const newNote = {
+          title: "یادداشت جدید",
+          body: `یه چیزی رو یادداشت کن...
+(راستی برای اعمال تغییرات کافیه بیرون باکس ها رو کلیک یا لمس کنی)`,
+        };
+        NotesAPI.saveNote(newNote);
         this._refreshNotes();
-    }
+      },
+      onNoteEdit: (newTitle, newBody) => {
+        NotesAPI.saveNote({
+          id: this.activeNote.id,
+          title: newTitle,
+          body: newBody,
+        });
 
-    _refreshNotes() {
-        const notes = NotesAPI.getAllNotes();
-
-        //set notes:
-        this._setNotes(notes);
-
-        //set active note:
-        if (notes.length > 0) {
-            this._setActiveNote(notes[0]);
-        }
-    }
-
-    _setNotes(notes) {
-        this.notes = notes;
-        this.view.updateNoteList(notes);
-        this.view.updateNotePreviewVisiblity(notes.length > 0);
-    }
-
-    _setActiveNote(note) {
-        this.activeNote = note;
-        this.view.updateActiveNote(note);
-    }
-
-    _handlers() {
-        return {
-            onNoteAdd: () => {
-                const newNote = {
-                    title: "یادداشت جدید",
-                    body: "یه چیزی رو یادداشت کن..."
-                };
-                NotesAPI.saveNote(newNote);
-                this._refreshNotes();
-            },
-            onNoteEdit: (newTitle, newBody) => {
-                NotesAPI.saveNote({
-                    id: this.activeNote.id,
-                    title: newTitle,
-                    body: newBody
-                });
-
-                this._refreshNotes();
-
-            },
-            onNoteSelect: (noteId) => {
-                const selectedNote = this.notes.find(n => n.id == noteId);
-                this._setActiveNote(selectedNote);
-            },
-            onNoteDelete: (noteId) => {
-                NotesAPI.deleteNote(noteId);
-                this._refreshNotes();
-            }
-        }
-    }
+        this._refreshNotes();
+      },
+      onNoteSelect: (noteId) => {
+        const selectedNote = this.notes.find((n) => n.id == noteId);
+        this._setActiveNote(selectedNote);
+      },
+      onNoteDelete: (noteId) => {
+        NotesAPI.deleteNote(noteId);
+        this._refreshNotes();
+      },
+    };
+  }
 }
