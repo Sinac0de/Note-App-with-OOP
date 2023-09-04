91fd9662b5e77cda0154b8536b2ba5fa1a2fd315
Sina Moradian <sinacsharp@gmail.com>
2023-09-05 02:59:44 +0330

    Refactor empty section

--- js/NotesView.js
+++ js/NotesView.js
@@ -138,17 +138,17 @@ export default class NotesView {
       ? "visible"
       : "hidden";
 
+    //check for empty section
+    const mainContent = this.root.querySelector(".main");
     if (visible === false) {
       const div = document.createElement("div");
       div.className = "empty-preview";
       div.innerHTML = `
       <h2>لطفا از قسمت منو یک یادداشت جدید ایجاد کنید 👉🏻</h2>`;
-      this.root.querySelector(".main").appendChild(div);
+      mainContent.appendChild(div);
     } else {
       if (this.root.querySelector(".empty-preview")) {
-        this.root
-          .querySelector(".main")
-          .removeChild(this.root.querySelector(".empty-preview"));
+        mainContent.removeChild(this.root.querySelector(".empty-preview"));
       }
     }
   }
